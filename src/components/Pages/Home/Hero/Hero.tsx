d9797af2d222b7c929535ede26062ddc98893778
Jesse Lind <52766281+jclind@users.noreply.github.com>
2025-07-22 12:56:37 -0400

    build: desktop homepage hero layout

--- src/components/Pages/Home/Hero/Hero.tsx
+++ src/components/Pages/Home/Hero/Hero.tsx
@@ -3,19 +3,52 @@ import styles from './Hero.module.scss'
 import { ButtonLink } from '../../../Common/ButtonLink'
 const Hero = () => {
   return (
-    <div className={`${styles.hero} page__horizontal-spacing-3xl`}>
-      <div className='page__inner'>
-        <div className={styles.content}>
-          <div className='never_final'>never final</div>
-          <div className={styles.overlapping_logo}>
-            <img
-              src='/images/jesselind_overlapping.png'
-              alt='Jesse Lind Logo'
-              className={styles.logo}
-            />
-          </div>
+    <div className={`${styles.hero}`}>
+      <div className={styles.content}>
+        <h1 className={`${styles.nav_link} nav_link`}>never final</h1>
+
+        <div className={styles.overlapping_logo}>
+          <img
+            src='/images/jesselind_overlapping.png'
+            alt='Jesse Lind Logo'
+            draggable='false'
+            className={styles.logo}
+          />
         </div>
       </div>
+      <div className={styles.lines}>
+        <span className={styles.line1}></span>
+        <span className={styles.line2}></span>
+        <span className={styles.line3}></span>
+        <span className={styles.line4}></span>
+        <div className={styles.arrows}>
+          <span className={styles.v1}></span>
+          <span className={styles.v2}></span>
+          <span className={styles.v3}></span>
+          <span className={styles.v4}></span>
+          <span className={styles.v5}></span>
+          <span className={styles.v6}></span>
+        </div>
+      </div>
+      <div className={styles.links}>
+        <a className={`${styles.notes} nav_link`}>
+          n<br />
+          o<br /> t<br /> e<br />s
+        </a>
+        <a className={`${styles.projects} nav_link`}>
+          p<br />
+          r<br /> o<br /> j<br /> e<br /> c<br /> t<br /> s
+        </a>
+        <a className={`${styles.logo} nav_link`}>
+          <img
+            src='/images/jesselind_kanji_logo.png'
+            alt='jesse lind kanji logo'
+            draggable='false'
+          />
+        </a>
+      </div>
+
+      {/* <div className={styles.mid_line}></div> */}
     </div>
   )
 }
---
+++ src/components/Pages/Home/Projects/Projects.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from './Projects.module.scss'
+
+const Projects = () => {
+  return (
+    <div className={styles.Projects}>
+      <div className='page__inner'>
+        <div className={styles.content}>testing</div>
+      </div>
+    </div>
+  )
+}
+
+export default Projects
