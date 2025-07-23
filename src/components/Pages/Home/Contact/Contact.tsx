f7600c87dc01edeabebe545dd4ca53bf32eb6932
Jesse Lind <jesse@Banana-Book.local>
2025-07-23 16:05:11 -0400

    build: add contact section boilderplate to home page

---
+++ src/components/Pages/Home/Contact/Contact.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from './Contact.module.scss'
+
+const Contact = () => {
+  return (
+    <div className={styles.Contact}>
+      <div className={styles.content}>
+        <h1>let’s make something</h1>
+        <div className={styles.contact_btn}></div>
+      </div>
+    </div>
+  )
+}
+
+export default Contact
