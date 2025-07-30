13b3effb9db8c8de9ec99a48b82ad80268b97609
Jesse Lind <jesse@Banana-Book-2.local>
2025-07-30 16:42:23 -0400

    build: update 404 page styles

--- src/components/Pages/PageNotFound/PageNotFound.tsx
+++ src/components/Pages/PageNotFound/PageNotFound.tsx
@@ -5,17 +5,10 @@ import { ButtonLink } from '../../Common/ButtonLink'
 const PageNotFound = () => {
   return (
     <div className={styles.page_not_found}>
-      <div className='page__inner'>
-        <div className={styles.container}>
-          <div className={styles.image}>
-            <img src='/images/page_not_found.svg' alt='Page Not Found' />
-          </div>
-
-          <div className={styles.content}>
-            <h1>Page Not Found</h1>
-            <p>The page you are looking for does not exist.</p>
-            <ButtonLink link='/' text='&larr; Back Home' size='large' />
-          </div>
+      <div className={styles.content}>
+        <div className={styles.content}>
+          <h1>page not found</h1>
+          <a href='/'>back</a>
         </div>
       </div>
     </div>
