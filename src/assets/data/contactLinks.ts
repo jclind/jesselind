40e45593dee8135ed91812ee14864cf7c41ffada
Jesse Lind <jesseclind@gmail.com>
2025-09-29 16:12:01 -0400

    feat: add more cloud images

--- src/assets/data/contactLinks.ts
+++ src/assets/data/contactLinks.ts
@@ -1,7 +1,7 @@
 export const contactLinks = [
   { text: 'email', src: 'mailto:jesselindcs@gmail.com' },
   { text: 'instagram', src: 'https://www.instagram.com/jclind02/' },
-  { text: 'photography', src: 'https://jesselindphotography.netlify.app/' },
+  { text: 'photography', src: 'https://jesselindphoto.vercel.app/' },
   { text: 'coffee', src: 'https://buymeacoffee.com/jesseclind' },
   { text: 'linkedin', src: 'https://www.linkedin.com/in/jesseclind/' },
   { text: 'github', src: 'https://github.com/jclind' },
--- src/components/Pages/Nurture/Nurture.tsx
+++ src/components/Pages/Nurture/Nurture.tsx
@@ -7,8 +7,20 @@ const Nurture = () => {
   return (
     <div className={styles.Nurtures}>
       <Lines />
-      <div className={styles.imageContainer}>
+      {/* <div className={styles.imageContainer}>
         <img src='/images/nurture/clouds.webp' alt='clouds and blue sky' />
+      </div> */}
+      <div className={styles.cloudImages}>
+        <img src='/images/nurture/clouds1.webp' alt='clouds and blue sky' />
+        <img src='/images/nurture/clouds2.webp' alt='clouds and blue sky' />
+        <img src='/images/nurture/clouds3.webp' alt='clouds and blue sky' />
+        <img src='/images/nurture/clouds4.webp' alt='clouds and blue sky' />
+        <img src='/images/nurture/clouds5.webp' alt='clouds and blue sky' />
+        <img src='/images/nurture/clouds6.webp' alt='clouds and blue sky' />
+        <img src='/images/nurture/clouds7.webp' alt='clouds and blue sky' />
+        <img src='/images/nurture/clouds8.webp' alt='clouds and blue sky' />
+        <img src='/images/nurture/clouds9.webp' alt='clouds and blue sky' />
+        <img src='/images/nurture/clouds10.webp' alt='clouds and blue sky' />
       </div>
     </div>
   )
